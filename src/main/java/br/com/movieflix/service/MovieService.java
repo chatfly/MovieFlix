ce871bbcd19c724a823fee03c42f7594b04af5d2
chatfly <oiamigopao@outlook.com>
2025-02-20 19:44:39 -0300

    feat: Cria as endpoints de Atualizar os dados de um filme, deletar e achar filmes pela categoria que ele pertence

--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -1,24 +1,83 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
 import br.com.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll() {
         return movieRepository.findAll();
+    }
+
+    public List<Movie> findByCategory(Long categoryId) {
+        return movieRepository.findMovieByCategories(List.of(Category.builder().id(categoryId).build()));
+    }
+
+    public Optional<Movie> findById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> update(Long id, Movie updateMovie) {
+        Optional<Movie> optMovie = movieRepository.findById(id);
+        if(optMovie.isPresent()){
+
+            List<Category> categories = this.findCategories(updateMovie.getCategories());
+            List<Streaming> streamings = this.findStreamings(updateMovie.getStreamings());
+
+            Movie movie = optMovie.get();
+            movie.setTitle(updateMovie.getTitle());
+            movie.setDescription(updateMovie.getDescription());
+            movie.setReleaseDate(updateMovie.getReleaseDate());
+            movie.setRating(updateMovie.getRating());
+
+            movie.getCategories().clear();
+            movie.getCategories().addAll(categories);
+
+            movie.getStreamings().clear();
+            movie.getStreamings().addAll(streamings);
+
+            movieRepository.save(movie);
+
+            return Optional.of(movie);
+        }
+
+        return Optional.empty();
+    }
+
+    public void delete(Long id) {
+        movieRepository.deleteById(id);
+    }
+
+    private List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> categoryService.findById(category.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
     }
 
 }
