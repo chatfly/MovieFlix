d81b1ee4670a495e962af2d1f39737ee4b2ed162
chatfly <oiamigopao@outlook.com>
2025-02-27 17:29:13 -0300

    docs: Adiciona a dependência do OpenAPI Swagger para a documentação da API.

---
+++ src/main/java/br/com/movieflix/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package br.com.movieflix.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI getOpenAPI() {
+
+        Contact contact = new Contact();
+        contact.name("Gabriel");
+        contact.email("gabrielmartcastro@gmail.com");
+        contact.setUrl("http://github.com/chatfly");
+
+        Info info = new Info();
+        info.title("Movieflix");
+        info.version("v1");
+        info.description("Aplicação para gerenciamento de catálogo de filmes");
+        info.contact(contact);
+
+        return new OpenAPI().info(info);
+
+    }
+
+}
